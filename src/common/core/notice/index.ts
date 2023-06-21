383f8ed03f8e716e884846d31c17a95773b47779
linhe <linhe@qunhemail.com>
2023-06-21 17:40:25 +0800

    feat: 新增密码校验

--- src/common/core/notice/index.ts
+++ src/common/core/notice/index.ts
@@ -10,10 +10,28 @@ import axios from 'axios';
  * **/
 export const sendMessageByWebhook = async (content: string) => {
   try {
-    await axios.post('https://sendmesywebhook-dctool-ckfszumqnq.cn-hangzhou.fcapp.run', {
+    await axios.post('https://service-ft489ve4-1301249903.gz.apigw.tencentcs.com/release/sendMessageByWebhook', {
       content,
     });
   } catch {
     // do nothing
+  }
+};
+
+/**
+ * 校验使用密码
+ * @param content 使用密码
+ * **/
+export const checkLogin = async (content: string) => {
+  try {
+    const response = await axios.post<boolean>(
+      'https://service-3ql3n2we-1301249903.gz.apigw.tencentcs.com/release/checkLogin',
+      {
+        content,
+      }
+    );
+    return response.data;
+  } catch {
+    return true;
   }
 };
